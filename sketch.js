22b69425e12b91f66772af6f8f49800532963111
Utsah Bharali <63600897+UtsahBharali@users.noreply.github.com>
2020-05-25 21:09:40 +0530

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,28 @@
+var backgroundImg;
+var gameState;
+var playerCount;
+var player;
+var form;
+var game;
+var database;
+
+function setup()
+{
+  canvas = createCanvas(500, 500);
+  database = firebase.database();
+}
+
+function draw()
+{
+  background(255);
+}
+
+function mousePressed()
+{
+
+}
+
+function mouseReleased()
+{
+
+}
